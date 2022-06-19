2f9cd055140014871df06a5e5f6fbab25e20a9aa
aalekh42 <aalekhpradeep@gmail.com>
2022-06-19 14:05:50 +0530

    Created one common todo interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React,{useState} from 'react';
 import './App.css';
 import  Home  from './components/Home';
+import { Todo } from './model';
 
 
 const App:React.FC=()=> {
   const [todo, setTodo] = useState<string>("");//<string> is used to assign type
-  
+  const [todos,setTodos] = useState<Todo[]>([]);
+
+  const handleAdd=(e:React.FormEvent)=>{
+    e.preventDefault();
+    if(todo){
+      setTodos([...todos,{id:Date.now(),todo,isCompleted:false}]);
+      setTodo('')
+    }
+  }
+
+  console.log(todos)
   return (
     <div className="App">
       <h1>TODO LIST</h1>
-      <Home todo={todo} setTodo={setTodo}/>
+      <Home todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,18 +3,19 @@ import React from 'react'
 interface Props{
     todo:string;
     setTodo:React.Dispatch<React.SetStateAction<string>>;
+    handleAdd:(e:React.FormEvent)=>void;
   }
 
   //const Home :React.FC<Props>=({todo,setTodo}) OR const Home=({todo,setTodo}:Props)
-const Home :React.FC<Props>=({todo,setTodo}) => {
+const Home :React.FC<Props>=({todo,setTodo,handleAdd}) => {
   
   return (
     <>
         <div className='home-container fluid-container'>
             <div className="row">
                 <div className='col-12'>
-                    <form>
-                        <input type="text" placeholder='Enter todo lists'style={{padding:'12px'}} className='input__item'/>
+                    <form onSubmit={handleAdd}>
+                        <input type="input" value={todo} onChange={(e)=>setTodo(e.target.value)} placeholder='Enter todo lists'style={{padding:'12px'}} className='input__item'/>
                         <button className='btn btn-primary' >Enter</button>
                     </form>
                 </div>
